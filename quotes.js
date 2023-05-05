ccdeefa3a77cdcf821c3c8bf2d4744f80de4dbbe
Andrew Lee <119084950+leeanddrew@users.noreply.github.com>
2023-05-05 12:43:13 -0400

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,12 @@
+const quotes = [
+"Happiness depends upon ourselves. —Aristotle", 
+"Happiness is when what you think, what you say, and what you do are in harmony. —Mahatma Gandhi", 
+"The moments of happiness we enjoy take us by surprise. It is not that we seize them, but that they seize us. —Ashley Montagu",  
+"Even if happiness forgets you a little bit, never completely forget about it. —Jaques Prevert",  
+"One of the secrets of a happy life is continuous small treats. —Iris Murdoch",
+"The only joy in the world is to begin. —Cesare Pavese",
+"It is only possible to live happily ever after on a daily basis. —Margaret Bonanno",
+"The pleasure which we most rarely experience gives us greatest delight. —Epictetus",
+"Remember this, that very little is needed to make a happy life. — Marcus Aurelius",
+"I wake up every morning with a great desire to live joyfully. — Anna Howard Shaw"
+];
